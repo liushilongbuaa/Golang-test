f2088824130c1e6434aa1c18c7b970e356eae082
[liushilong5] <[liushilong@jd.com]>
2019-10-29 15:36:37 +0800

    fix

--- golang/pachong/run.go
+++ golang/pachong/run.go
@@ -8,13 +8,13 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
-	//	"github.com/axgle/mahonia"
+	"github.com/axgle/mahonia"
 )
 
 func main() {
-	path := "qindi1.txt"
-	baseurl := "https://www.tianxiabachang.cn"
-	url := baseurl + "/5_5593/2268166.html"
+	path := "jinyiyexing.txt"
+	baseurl := "https://www.biquge.cm"
+	url := baseurl + "/3/3957/2295247.html"
 	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 777)
 	if err != nil {
 		fmt.Println("OpenFile: ", err)
@@ -28,46 +28,47 @@ func main() {
 		start := time.Now()
 		cli := &http.Client{Timeout: time.Duration(10) * time.Second}
 		resp := &http.Response{}
+		var doc *goquery.Document
 		for {
 			resp, err = cli.Get(url)
-			if err == nil && !resp.Close {
-				break
+			if err != nil {
+				time.Sleep(time.Second)
+				continue
 			}
-			fmt.Println(err, resp)
+			doc, err = goquery.NewDocumentFromReader(resp.Body)
+			if err != nil {
+				fmt.Println("NewDocumentFromR.eader: ", err)
+				continue
+			}
+			defer resp.Body.Close()
+			break
 		}
-
 		fmt.Printf("%d, %v", i, time.Since(start))
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
-		if err != nil {
-			fmt.Println("NewDocumentFromR.eader: ", err)
-			return
-		}
-		resp.Body.Close()
-		boxCon := doc.Find(".box_con")
 
-		// get title
-		titlestrs := boxCon.Find(".bookname").Text()
-		ts := strings.Split(titlestrs, "\n")
-		var title, body string
-		for _, line := range ts {
-			if strings.Contains(line, "第") && strings.Contains(line, "章") {
-				title = line[strings.Index(line, "第"):]
-				title = title + "\n"
-				break
+		boxCon := doc.Find(".box_con")
+		var decode func(string) string
+		if true {
+			decode = func(a string) string {
+				return mahonia.NewDecoder("gbk").ConvertString(a)
+			}
+		} else {
+			decode = func(a string) string {
+				return a
 			}
 		}
+		// get title
+		title := decode(boxCon.Find(".bookname").Find("h1").Text()) + "\n\n"
+		xiayizhang := "a:contains(" + mahonia.NewEncoder("gbk").ConvertString("下一章") + ")"
+		url = baseurl + boxCon.Find(".bottem1").Find(xiayizhang).Nodes[0].Attr[0].Val
 
-		url = baseurl + boxCon.Find(".bottem1").Find("a:contains(下一章)").Nodes[0].Attr[0].Val
-
-		body = boxCon.Find("#content").Text() + "\n\n"
-		//	body = mahonia.NewDecoder("gbk").ConvertString(body)
-
+		body := decode(boxCon.Find("#content").Text()) + "\n\n"
+		title = strings.Replace(title, "聽聽聽聽", "", -1)
+		body = strings.Replace(body, "聽聽聽聽", "", -1)
 		f.WriteString(title)
 		f.WriteString(body)
 
 		fmt.Printf("  %v", time.Since(start))
 		fmt.Println("")
 		time.Sleep(time.Second * time.Duration(2))
-
 	}
 }
